9631d7ecc4d06c2a27feaf6315eea467c78233dc
B Leung <leungcheukkei@gmail.com>
2021-01-05 01:31:52 +0800

    commit before fetching profile with redux

--- server/migrations/20201230163517_users_projects.js
+++ server/migrations/20201230163517_users_projects.js
@@ -4,10 +4,14 @@ exports.up = function(knex, Promise) {
     .createTable('users_projects', function(table) {
       table.increments('project_id').primary();
       table.integer('users_id');
-      table.string('project_img_url');
+      table.string('project_img_url1');
+      table.string('project_img_url2');
+      table.string('project_img_url3');
       table.string('project_title');
-      table.string('project_summary');
+      table.string('project_summary1');
+      table.string('project_summary2');
       table.string('project_url');
+      table.string('project_tags');
     });    
 };
 
--- server/routers/routerUsers.js
+++ server/routers/routerUsers.js
@@ -67,6 +67,74 @@ router.post('/getOwnProfile', async (req, res) => {
 }   
 })
     
+
+//get other profile
+router.post('/getProfile/:id', async (req, res) => { 
+
+    // console.trace(req.params, req.body)
+    let userId = parseInt(req.params.id); //the id of the profile
+    let accessToken = req.body.accessToken; // access token of requester
+    console.trace(userId)
+
+    try {
+        let requester = jwt.verify(accessToken, config.jwtSecret);
+        console.trace(requester, requester.id)
+
+        let profileUser = await knex.from('users').where('id','=',userId);
+            
+        if (profileUser[0]) {
+            let profileUserData = await knex('users').where('id', userId);
+            let projects = await knex('users_projects').where('users_id', userId)
+            let following = await knex('users_follows').where('users_id', userId).andWhere('followers_id', requester.id)
+
+            let { full_name, user_img_url, company, job_title, location, skills, github_url, facebook_url, twitter_url,linkedin_url, website_url, summary} = profileUserData[0];
+
+            let sameUser, isFollowing;
+
+            if (userId === requester.id) {
+                sameUser = true
+            } else {
+                sameUser = false
+            }
+
+            if (following.length > 0) {
+                isFollowing = true
+            } else {
+                isFollowing = false
+            }
+
+            res.json({
+                full_name,
+                user_img_url,
+                company,
+                job_title,
+                location,
+                skills,
+                github_url,
+                facebook_url,
+                twitter_url,
+                linkedin_url,
+                website_url,
+                summary,
+                projects,
+                sameUser,
+                isFollowing
+            })
+
+        } else {
+            res.json({
+                message: "The user you have requested is not found. "
+            })
+        }
+        
+
+    } catch (err) {
+        console.trace(err)
+        res.sendStatus(400)
+}   
+
+})
+    
 //get own projects
 router.post('/getOwnProjects', async (req, res) => {
 
@@ -96,9 +164,258 @@ router.post('/getOwnProjects', async (req, res) => {
 }   
 })
 
-    
-    
+//update profile
+    router.post('/updateProfile', async (req, res) => {
+        console.log(req.body.userData)
+        let userId = req.body.userData.id
 
+        try {
+
+            let userData = {};
+            
+                if(req.body.userData.user_img_url && req.body.userData.user_img_url != ''){
+                    let changeProfilePicture = await knex.update('user_img_url', req.body.userData.user_img_url)
+                    .from('users').where('id', '=', userId).returning('user_img_url')
+                    
+                    userData.user_img_url = changeProfilePicture[0]
+
+                    console.trace(changeProfilePicture) 
+                } else if (req.body.userData.summary && req.body.userData.summary != '') {
+                    let changeSummary = await knex.update('summary', req.body.userData.summary)
+                    .from('users').where('id', '=', userId).returning('summary')
+                    console.trace(changeSummary)
+                    
+                    userData.summary = changeSummary[0]
+
+                    console.trace(changeSummary) 
+                } else {
+                if (req.body.userData.full_name != '') {
+                    let changeFullName = await knex.update('full_name', req.body.userData.full_name)
+                    .from('users').where('id', '=', userId).returning('full_name')
+                    
+                    userData.full_name = changeFullName[0]
+
+                    console.trace(changeFullName) 
+                }
+
+                if (req.body.userData.email != '') {
+                    let checkEmail = await knex('users').where('email', req.body.userData.email)
+
+                    if (checkEmail.length > 0) {
+                        userData.errorMessage = "Email already registered."
+                    } else {
+                        let changeEmail = await knex.update('email', req.body.userData.email)
+                        .from('users').where('id', '=', userId).returning('email')
+
+                        userData.email = changeEmail[0]
+                        console.trace(changeEmail)
+                    }
+                    
+                } 
+
+                if (req.body.userData.password != '') {
+                    let hashedPassword = await bcrypt.hashPassword(req.body.userData.password)
+                    console.trace(hashedPassword);
+
+                    let changePassword = await knex('users').where('id', '=', userId).update({
+                        'password': hashedPassword,
+                    })
+
+                    console.trace(changePassword)
+                }
+
+
+                if (req.body.userData.company != '') {
+                    let changeCompany = await knex.update('company', req.body.userData.company)
+                    .from('users').where('id', '=', userId).returning('company')
+                    
+                    userData.company = changeCompany[0]
+
+                    console.trace(changeCompany) 
+                } 
+                
+                if (req.body.userData.job_title != '') {
+                    let changeJobTitle = await knex.update('job_title', req.body.userData.job_title)
+                    .from('users').where('id', '=', userId).returning('job_title')
+
+                    userData.job_title = changeJobTitle[0]
+
+                    console.trace(changeJobTitle) 
+                } 
+                
+                if (req.body.userData.location != '') {
+                    let changeLocation = await knex.update('location', req.body.userData.location)
+                    .from('users').where('id', '=', userId).returning('location')
+                    
+                    userData.location = changeLocation[0]
+
+                    console.trace(changeLocation) 
+                } 
+
+                if (req.body.userData.website_url != '') {
+                    let website_url;
+                    let prefix = "http://"
+                    req.body.userData.website_url.toLowerCase().includes(prefix && "https://") ?
+                        (website_url = req.body.userData.website_url) : (website_url = prefix.concat(req.body.userData.website_url))
+
+                    let changeWebsiteUrl = await knex.update('website_url', website_url)
+                    .from('users').where('id', '=', userId).returning('website_url')
+                    
+                    userData.website_url = changeWebsiteUrl[0]
+
+                    console.trace(changeWebsiteUrl) 
+                } 
+
+                if (req.body.userData.github_url != '') {
+                    let github_url;
+                    let prefix = "http://"
+                    req.body.userData.github_url.toLowerCase().includes(prefix && "https://") ?
+                        (github_url = req.body.userData.github_url) : (github_url = prefix.concat(req.body.userData.github_url))
+
+                    let changeGithubUrl = await knex.update('github_url', github_url)
+                    .from('users').where('id', '=', userId).returning('github_url')
+                    
+                    userData.github_url = changeGithubUrl[0]
+
+                    console.trace(changeGithubUrl) 
+                }
+
+                if (req.body.userData.linkedin_url != '') {
+                    let linkedin_url;
+                    let prefix = "http://"
+                    req.body.userData.linkedin_url.toLowerCase().includes(prefix && "https://") ?
+                        (linkedin_url = req.body.userData.linkedin_url) : (linkedin_url = prefix.concat(req.body.userData.linkedin_url))
+
+                    let changeLinkedinUrl = await knex.update('linkedin_url', linkedin_url)
+                    .from('users').where('id', '=', userId).returning('linkedin_url')
+                    
+                    userData.linkedin_url = changeLinkedinUrl[0]
+
+                    console.trace(changeLinkedinUrl) 
+                } 
+
+                if (req.body.userData.twitter_url != '') {
+                    let twitter_url;
+                    let prefix = "http://"
+                    req.body.userData.twitter_url.toLowerCase().includes(prefix && "https://") ?
+                        (twitter_url = req.body.userData.twitter_url) : (twitter_url = prefix.concat(req.body.userData.twitter_url))
+
+                    let changeTwitterUrl = await knex.update('twitter_url', twitter_url)
+                    .from('users').where('id', '=', userId).returning('twitter_url')
+                    
+                    userData.twitter_url = changeTwitterUrl[0]
+
+                    console.trace(changeTwitterUrl) 
+                } 
+
+                if (req.body.userData.facebook_url != '') {
+                    let facebook_url;
+                    let prefix = "http://"
+                    req.body.userData.facebook_url.toLowerCase().includes(prefix && "https://") ?
+                        (facebook_url = req.body.userData.facebook_url) : (facebook_url = prefix.concat(req.body.userData.facebook_url))
+
+                    let changeFacebookUrl = await knex.update('facebook_url', facebook_url)
+                    .from('users').where('id', '=', userId).returning('facebook_url')
+                    
+                    userData.facebook_url = changeFacebookUrl[0]
+
+                    console.trace(changeFacebookUrl) 
+                } 
+
+
+                
+                if (req.body.userData.skills != '') {
+                    // console.trace(req.body.userData.skills)
+                    let mappedSkills = req.body.userData.skills.map(x => x.skill)
+                    // console.trace(mappedSkills)
+                    let stringSkills = mappedSkills.toString()
+                    // console.trace(stringSkills)
+                        
+                    let changeSkills = await knex.update('skills', stringSkills)
+                    .from('users').where('id', '=', userId).returning('skills')
+                    
+                    userData.skills = changeSkills[0]
+
+                    console.trace(changeSkills) 
+                } 
+                }
+
+                console.trace(userData)
+                res.json(userData)
+            
+        } catch (err) {
+            console.trace(err);
+            res.sendStatus(400)
+        }
+    })
+    
+    //follow/unfollow
+router.post('/follow/:id', async (req, res) => {
+    // console.trace(req.params, req.body)
+    let userId = parseInt(req.params.id); //the id of the profile
+    let accessToken = req.body.accessToken; // access token of requester
+    console.trace(userId)
+
+    try {
+        let requester = jwt.verify(accessToken, config.jwtSecret);
+        console.trace(requester, requester.id)
+
+        let checkFollowing = await knex.from('users_follows').where('users_id','userId').andWhere('followers_id',requester.id)
+        
+        //!
+        let profileUser = await knex.from('users').where('id', '=', userId);
+            
+        if (profileUser[0]) {
+            let profileUserData = await knex('users').where('id', userId);
+            let projects = await knex('users_projects').where('users_id', userId)
+            let following = await knex('users_follows').where('users_id', userId).andWhere('followers_id', requester.id)
+
+            let { full_name, user_img_url, company, job_title, location, skills, github_url, facebook_url, twitter_url,linkedin_url, website_url, summary} = profileUserData[0];
+
+            let sameUser, isFollowing;
+
+            if (userId === requester.id) {
+                sameUser = true
+            } else {
+                sameUser = false
+            }
+
+            if (following.length > 0) {
+                isFollowing = true
+            } else {
+                isFollowing = false
+            }
+
+            res.json({
+                full_name,
+                user_img_url,
+                company,
+                job_title,
+                location,
+                skills,
+                github_url,
+                facebook_url,
+                twitter_url,
+                linkedin_url,
+                website_url,
+                summary,
+                projects,
+                sameUser,
+                isFollowing
+            })
+
+        } else {
+            res.json({
+                message: "The user you have requested is not found. "
+            })
+        }
+        
+
+        } catch (err) {
+            console.trace(err)
+            res.sendStatus(400)
+    }   
+    })
 
 
     return router; 
